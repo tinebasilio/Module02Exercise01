c82be381d8547b87cf5480cbf75eca338abaaf92
tinebasilio <kristinebasilio28@gmail.com>
2024-08-28 13:49:05 +0800

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Module02Exercise01
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
